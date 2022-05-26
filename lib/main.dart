3d54c73fe4e5c66f977312700f363c26ac107dfb
jobroloff <jobroloff@gmail.com>
2022-05-26 18:54:35 -0500

    added provider dependency & multi provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,27 @@
+import 'dart:js';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_application_note/pages.dart';
+import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 
 import 'package:flutter/material.dart';
 
+import 'providers/googlesignin.dart';
 import 'user_screening.dart';
 
 Future<void> main() async {
   
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context)=> GoogleSignInProvider()),
+      ],
+      child: MyApp()
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
