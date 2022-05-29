6a8e995bc5d327816271a153f56bbe655e6d33c6
JobRoloff <61367131+JobRoloff@users.noreply.github.com>
2022-05-28 19:03:51 -0500

    Update README.md

---
+++ lib/providers/navlocation.dart
@@ -0,0 +1,10 @@
+
+import 'dart:collection';
+
+import 'package:flutter/cupertino.dart';
+
+class NavLocationProvider with ChangeNotifier{
+  int navLocation = 1;
+  HashMap<String, Object> navLocationMap = HashMap();
+  
+}
---
+++ lib/styles.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+final ColorScheme colorScheme1 = ColorScheme(
+    brightness: Brightness.dark,
+    primary: brown500,
+    onPrimary: onPrimary,
+    secondary: secondary,
+    onSecondary: onSecondary,
+    error: error,
+    onError: onError,
+    background: background,
+    onBackground: onBackground,
+    surface: surface,
+    onSurface: onSurface);
+
+//Primary
+var brown50 = const Color(0xFFEFEBE9);
+var brown100 = const Color(0xFFD7CCC8);
+var brown200 = const Color(0xFFBCAAA4);
+var brown300 = const Color(0xFFA1887F);
+var brown400 = const Color(0xFF8D6E63);
+var brown500 = const Color(0xFF795548); //Primary
+var brown600 = const Color(0xFF6D4C41);
+var brown700 = const Color(0xFF5D4037); //things like status bar
+var brown800 = const Color(0xFF4E342E);
+var brown900 = const Color(0xFF3E2723);
+
+//primary light
+var bpl = const Color(0xFFa98274);
+//primary dark
+var vpd = const Color(0xFF4b2c20);
+//primary text
+var ptx = const Color(0xFFffffff);
+
+//Complimentary
+
+var bg50 = const Color(0xFFE6F0F5);
+var bg100 = const Color(0xFFA7BDC6);
+var bg300 = const Color(0xFF86A3AE);
+var bg400 = const Color(0xFF6D8E9C);
+var bg500 = const Color(0xFF547B8A);
+var bg600 = const Color(0xFF486C79);
+var bg700 = const Color(0xFF3A5863);
+var bg800 = const Color(0xFF2C454F);
+var bg900 = const Color(0xFF1C3038);
+
+// Analogous
+
+var pa100 = const Color(0xFFC7CCD9);
+var pa200 = const Color(0xFFA4ACBF);
+var pa400 = const Color(0xFF687382);
+var pa600 = const Color(0xFF485379);
+var pa700 = const Color(0xFF404A6D);
+
+var ca100 = const Color(0xFFBCDFDA);
+var ca200 = const Color(0xFF94CBC3);
+var ca400 = const Color(0xFF5EA69A);
+var ca700 = const Color(0xFF48796e);
+
+
+
