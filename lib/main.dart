5d4bbbe5a04b1e8601f5da5dd24d69bf28584816
job <jobroloff@gmail.com>
2022-06-21 14:19:35 -0700

    removing firebase stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,24 @@
 
 import 'dart:ui';
 
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_application_note/providers/sci_buff_provider.dart';
 import 'package:flutter_application_note/screens/data_screen.dart';
 import 'package:flutter_application_note/screens/portal_screen.dart';
 import 'package:flutter_application_note/screens/sci_screen.dart';
 import 'package:flutter_application_note/screens/user_settings_screen.dart';
 import 'package:provider/provider.dart';
-import 'firebase_options.dart';
 
 import 'package:flutter/material.dart';
 
 import 'styles/colorschemes.dart';
-import 'providers/googlesignin.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+
 
   runApp(
     MultiProvider(providers: [
       ChangeNotifierProvider(create: (context) => SciBuffProvider()),
-      ChangeNotifierProvider(create: (context) => GoogleSignInProvider()),
 
     ], child: const MyApp()),
   );
